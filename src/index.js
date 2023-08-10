b458187ad872c802ed81ad356163e750d7a374b5
unknown <m.n.volkova22@gmail.com>
2023-08-10 23:28:26 +0300

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
+    let str = n.toString();
+    let result = 0;
+    if(n >= 0)
+    {
+    for(let i = 0; i < str.length; i++) {
+        result = result + parseInt(str[i])*(10**i);
+    }
+   }
+   if(n< 0) {
+    for(let i = 1; i < str.length; i++) {
+        result = result + parseInt(str[i])*(10**(i-1));
+    }
+   }
+
+    return result;
   
 }
